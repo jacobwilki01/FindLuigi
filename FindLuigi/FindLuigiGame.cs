f97b41025bc0afc29218bc706e609b0b9b1ac616
Jacob Wilkus <jwilkus1@gmail.com>
2024-11-29 19:13:41 -0600

    Updates for final publish

--- FindLuigi/FindLuigiGame.cs
+++ FindLuigi/FindLuigiGame.cs
@@ -33,13 +33,21 @@ namespace FindLuigi
 
         private SpriteFont? _scoreFont;
 
+        private Difficulty _difficulty = Difficulty.EASY;
+
         private int _score = 0;
 
         private int _lives = 5;
 
         private int _time = 30;
 
-        private Difficulty _difficulty = Difficulty.EASY;
+        private int _highScore = 0;
+
+        private bool _gameOver = false;
+
+        private string _gameOverText = "GAME OVER!";
+
+        private string _restartText = "Press 'R' to restart.";
 
         public static int DifficultySpeedMultipler = 0;
 
@@ -81,7 +89,7 @@ namespace FindLuigi
 
         protected override void Update(GameTime gameTime)
         {
-            if (!_gameTimer!.Enabled)
+            if (!_gameOver && !_gameTimer!.Enabled)
                 _gameTimer.Start();
 
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -89,11 +97,12 @@ namespace FindLuigi
 
             foreach (Character character in _characters)
             {
-                character.Update();
+                if (!_gameOver)
+                    character.Update();
             }
 
             MouseState state = Mouse.GetState();
-            if (state.LeftButton == ButtonState.Pressed && (_timeout is null || !_timeout.Enabled))
+            if (state.LeftButton == ButtonState.Pressed && (_timeout is null || !_timeout.Enabled) && !_gameOver)
             {
                 Character? clickedCharacter = null;
                 List<Character> eligibleClicks = new();
@@ -133,18 +142,39 @@ namespace FindLuigi
                         UpdateDifficultyMultipliers();
 
                     GenerateRound();
-                    StartTimer();
+                    StartClickTimeout();
 
                     _time += 5;
                 }
                 else if (clickedCharacter is not null)
                 {
-                    if (_lives > 1)
-                        _lives--;
-
                     _characters.Remove(clickedCharacter);
-                    StartTimer();
+                    _lives--;
+
+                    if (_lives > 0)
+                    {
+                        StartClickTimeout();
+                    }
+                    else
+                    {
+                        _gameOver = true;
+                    }
                 }
+            }
+
+            if (_gameOver)
+                _highScore = Math.Max(_score, _highScore);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.R) && _gameOver)
+            {
+                _score = 0;
+                _time = 30;
+                _lives = 5;
+                _gameOver = false;
+                _difficulty = Difficulty.EASY;
+
+                UpdateDifficultyMultipliers();
+                GenerateRound();
             }
 
             base.Update(gameTime);
@@ -158,7 +188,7 @@ namespace FindLuigi
 
             foreach (Character character in _characters)
             {
-                if (_time > 0 || character.IsLuigi)
+                if (!_gameOver || character.IsLuigi)
                     character.Draw(_spriteBatch);
             }
 
@@ -174,6 +204,19 @@ namespace FindLuigi
             Vector2 timeOrigin = _scoreFont!.MeasureString(timeAsText) / 3;
             _spriteBatch.DrawString(_scoreFont!, timeAsText, new Vector2(15 + (7 * scoreAsText.Length), 115), Color.White, 0, timeOrigin, 3.0f, SpriteEffects.None, 0.5f);
 
+            if (_gameOver)
+            {
+                Vector2 gameOverOrigin = _scoreFont!.MeasureString(_gameOverText);
+                _spriteBatch.DrawString(_scoreFont!, _gameOverText, new Vector2(775, 500), Color.White, 0, gameOverOrigin, 5.0f, SpriteEffects.None, 0.5f);
+
+                Vector2 restartTextOrigin = _scoreFont!.MeasureString(_restartText);
+                _spriteBatch.DrawString(_scoreFont!, _restartText, new Vector2(720, 550), Color.White, 0, restartTextOrigin, 3.0f, SpriteEffects.None, 0.5f);
+
+                string highScoreAsText = "High Score: " + _highScore.ToString();
+                Vector2 highScoreOrigin = _scoreFont!.MeasureString(highScoreAsText) / 3;
+                _spriteBatch.DrawString(_scoreFont!, highScoreAsText, new Vector2(465, 575), Color.White, 0, highScoreOrigin, 3.0f, SpriteEffects.None, 0.5f);
+            }
+
             _spriteBatch!.End();
 
             base.Draw(gameTime);
@@ -184,11 +227,13 @@ namespace FindLuigi
         {
             _characters.Clear();
 
-            int totalSquares = random.Next(75, 100) * DifficultyCharacterCountMultiplier;
+            int totalCharacters = random.Next(75, 100) * DifficultyCharacterCountMultiplier;
             List<CharacterEnum> characterEnums = Enum.GetValues<CharacterEnum>().ToList();
             characterEnums.Remove(CharacterEnum.LUIGI);
 
-            for (int i = 0; i < totalSquares; i++)
+            int canMove = random.Next(0, 1 + DifficultyDirectionMultiplier);
+
+            for (int i = 0; i < totalCharacters; i++)
             {
                 int x = random.Next(1, 46) * 20;
                 int y = random.Next(1, 46) * 20;
@@ -202,8 +247,8 @@ namespace FindLuigi
 
                 Character character = new Character(x, y, characterEnums[characterType]);
 
-                character.xDirection = random.Next(-1, 2) * DifficultyDirectionMultiplier;
-                character.yDirection = random.Next(-1, 2) * DifficultyDirectionMultiplier;
+                character.xDirection = random.Next(-1, 2) * canMove;
+                character.yDirection = random.Next(-1, 2) * canMove;
                 character.xSpeed = random.Next(1, 3) * (1 + DifficultySpeedMultipler);
                 character.ySpeed = random.Next(1, 3) * (1 + DifficultySpeedMultipler);
 
@@ -211,8 +256,8 @@ namespace FindLuigi
             }
 
             Character luigi = new Character(random.Next(1, 46) * 20, random.Next(1, 46) * 20, CharacterEnum.LUIGI);
-            luigi.xDirection = random.Next(-1, 2) * DifficultyDirectionMultiplier;
-            luigi.yDirection = random.Next(-1, 2) * DifficultyDirectionMultiplier;
+            luigi.xDirection = random.Next(-1, 2) * canMove;
+            luigi.yDirection = random.Next(-1, 2) * canMove;
             luigi.xSpeed = random.Next(1, 3) * (1 + DifficultySpeedMultipler);
             luigi.ySpeed = random.Next(1, 3) * (1 + DifficultySpeedMultipler);
 
@@ -225,6 +270,9 @@ namespace FindLuigi
             switch(_difficulty)
             {
                 case Difficulty.EASY:
+                    DifficultySpeedMultipler = 0;
+                    DifficultyDirectionMultiplier = 0;
+                    DifficultyCharacterCountMultiplier = 1;
                     break;
                 case Difficulty.MEDIUM:
                     DifficultyCharacterCountMultiplier = 2;
@@ -232,20 +280,19 @@ namespace FindLuigi
                     break;
                 case Difficulty.HARD:
                     DifficultyCharacterCountMultiplier = 3;
-                    DifficultyDirectionMultiplier = 0;
                     break;
                 case Difficulty.ULTRA:
                     DifficultyCharacterCountMultiplier = 2;
-                    DifficultyDirectionMultiplier = 1;
                     DifficultySpeedMultipler = 2;
                     break;
                 case Difficulty.IMPOSSIBLE:
                     DifficultyCharacterCountMultiplier = 3;
+                    DifficultySpeedMultipler = 3;
                     break;
             }
         }
 
-        public void StartTimer()
+        public void StartClickTimeout()
         {
             _timeout = new(500);
             _timeout.Elapsed += OnTimeoutEvent!;
@@ -263,9 +310,19 @@ namespace FindLuigi
         public void GameTimerElapsed(object source, ElapsedEventArgs e)
         {
             Timer timer = (Timer)source;
-            timer.Stop();
-            if (_time > 0)
+
+            if (_time > 0 && !_gameOver)
+            {
                 _time--;
+            }
+            else
+            {
+                _gameOver = true;
+
+                timer.AutoReset = false;
+            }
+
+            timer.Stop();
         }
     }
 }
